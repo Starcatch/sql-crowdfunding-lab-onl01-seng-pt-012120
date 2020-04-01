e90bba7015ae1c6e6ef027eb8a83815a90d28ee0
Timur Mishiev <timurmishiev@gmail.com>
2020-04-01 01:11:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -29,3 +29,35 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Dog Harnesses", "Blind & Deaf", 500, "7/1/17", "7/31/17");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Musical Instruments", "Developmental Disabilities", 315, "7/1/17", "7/31/17");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("PTSD Counseling", "Military", 80, "7/1/17", "7/31/17");
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (35, 1, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (40, 1, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (25, 1, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (20, 2, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (20, 2, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 3, 8);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 3, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 4, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 5, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (35, 5, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 6, 7);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 6, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 6, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 6, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (25, 7, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (25, 7, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (100, 8, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (25, 8, 2);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (35, 9, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 10, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (30, 11, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (25, 12, 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 13, 10);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (10, 14, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (40, 15, 3);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (20, 16, 6);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (15, 17, 9);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (50, 18, 5);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (15, 19, 4);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (25, 20, 1);
+© 2020 GitHub, Inc.
